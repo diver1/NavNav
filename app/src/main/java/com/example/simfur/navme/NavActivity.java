4de03d6efc9a281d4a6a75979db0e418c357a76b
Simon Furborg <kattassen@gmail.com>
2015-07-09 21:20:38 +0200

    Added location code GPS and WIFI

--- app/src/main/java/com/example/simfur/navme/NavActivity.java
+++ app/src/main/java/com/example/simfur/navme/NavActivity.java
@@ -1,7 +1,12 @@
 package com.example.simfur.navme;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +17,30 @@ public class NavActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
+
+        /* Acquire a reference to the system Location Manager */
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+	    /* Define a listener that responds to location updates */
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                Log.d("This is tha latitude: ", Double.toString(location.getLatitude()));
+                //          makeUseOfNewLocation(location);
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+	    /* Register the listener with the Location Manager
+	     * to receive location updates */
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
     @Override
