0dd8470414e5a4f5bb8b303ac9e05c8c413f826e
Simon Furborg <kattassen@gmail.com>
2015-07-12 19:43:20 +0200

    Added POI class (place of interest)

---
+++ app/src/main/java/com/example/simfur/navme/POI.java
@@ -0,0 +1,24 @@
+package com.example.simfur.navme;
+
+public class POI {
+    private double lat;
+    private double lon;
+    private String name;
+    private int id;
+
+    public void setName(String type) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
