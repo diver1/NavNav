0e4f5df69d9e1f381923315e4d763eb536386e25
Simon Furborg <kattassen@gmail.com>
2015-07-27 22:47:13 +0200

    Removed obsolete imports

--- app/src/main/java/com/example/simfur/navme/TabsActivity.java
+++ app/src/main/java/com/example/simfur/navme/TabsActivity.java
@@ -2,14 +2,9 @@ package com.example.simfur.navme;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class TabsActivity extends FragmentActivity
         implements ActionBar.TabListener, routeFragment.OnFragmentInteractionListener {
