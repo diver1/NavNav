9b5b7fc4f475e344431eaece9bf672fe4e16dfb9
Simon Furborg <kattassen@gmail.com>
2015-08-03 21:25:02 +0200

    Added method getFragment

--- app/src/main/java/com/example/simfur/navme/TabsPagerAdapter.java
+++ app/src/main/java/com/example/simfur/navme/TabsPagerAdapter.java
@@ -4,20 +4,31 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class TabsPagerAdapter extends FragmentPagerAdapter {
+    private Map<Integer, Fragment> mPageReferenceMap = new HashMap<Integer, Fragment>();
+
     public TabsPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
     @Override
     public Fragment getItem(int index) {
+        /* Create fragments and add to hashmap */
+        Fragment fragment;
         switch (index) {
             case 0:
                 // List of routes
-                return new RouteListFragment();
+                fragment = new RouteListFragment();
+                mPageReferenceMap.put(index, fragment);
+                return fragment;
             case 1:
                 // Route player
-                return new NavFragment();
+                fragment = new NavFragment();
+                mPageReferenceMap.put(index, fragment);
+                return fragment;
         }
         return null;
     }
@@ -26,5 +37,10 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
     public int getCount() {
         /* Get item count - equal to number of tabs */
         return 2;
+    }
+
+    public Fragment getFragment(int key) {
+        /* Get fragment from key */
+        return mPageReferenceMap.get(key);
     }
 }
