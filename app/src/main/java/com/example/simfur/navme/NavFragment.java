c0db286d4fe923faaca44974f5cd1aa842393704
Simon Furborg <kattassen@gmail.com>
2015-08-04 10:20:21 +0200

    Removed obsolete CalcDistance method since location.distanceBetween is used instead

--- app/src/main/java/com/example/simfur/navme/NavFragment.java
+++ app/src/main/java/com/example/simfur/navme/NavFragment.java
@@ -148,19 +148,6 @@ public class NavFragment extends Fragment {
         void onFragmentInteraction(String id);
     }
 
-    private double calcDistance(double lat1, double lng1, double lat2, double lng2) {
-        /* Calculate distance in meter */
-        double earthRadius = 6371000;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                        Math.sin(dLng/2) * Math.sin(dLng/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-        return earthRadius * c;
-    }
-
     private void checkTTS(){
         // Check if TTS is available
         Intent check = new Intent();
