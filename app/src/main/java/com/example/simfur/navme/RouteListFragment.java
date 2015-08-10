4058a891b033af63d7aa238e18c09b2c77895078
Simon Furborg <kattassen@gmail.com>
2015-08-10 12:24:24 +0200

    Fixed bad handling of xml file name

--- app/src/main/java/com/example/simfur/navme/RouteListFragment.java
+++ app/src/main/java/com/example/simfur/navme/RouteListFragment.java
@@ -76,7 +76,7 @@ public class RouteListFragment extends Fragment implements AbsListView.OnItemCli
         if (null != mListener) {
             // Notify the active callbacks interface (the activity, if the
             // fragment is attached to one) that an item has been selected.
-            mListener.onFragmentInteraction(RouteFileList.ITEMS.get(position).id);
+            mListener.onFragmentInteraction(RouteFileList.ITEMS.get(position).content);
         }
     }
 
